079fc8dfb2b5b25d202b7393b3a425e3183d869f
Abhishek <abhishekyadav543.ay@gmail.com>
2021-11-19 23:35:48 +0530

    add a divide.java file

---
+++ divide.java
@@ -0,0 +1,11 @@
+public class sum{
+    public static void main(String args[]){
+        int a = 5;
+        int b= 6;
+
+        int c = a / b;
+
+        System.out.println("the multiply of two number is :"+ c);
+        System.out.println(" i have made change in this file ");
+    }
+}
